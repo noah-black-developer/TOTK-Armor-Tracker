77939d03c2300a658a4e13bdba9d057725d16ea2
Noah Black <57203908+ReactedTugBoat@users.noreply.github.com>
2024-04-07 14:41:41 -0400

    Added compression to build scripts
    - Changed name of raw application files from "TotkArmorTracker" to "project" to remove name conflicts with generated executables
    - Added --compression flag to build script

--- build.sh
+++ build.sh
@@ -1,6 +1,22 @@
 #!/bin/bash
 
+# Initialize bin directory, if not existing.
+echo Generating bin directory...
+mkdir -p bin
+cd bin
+
+# Clean and run qmake/make build steps.
+echo Building new exe...
 "/usr/bin/make" clean -j4
-"/opt/Qt/6.5.2/gcc_64/bin/qmake" /home/noah/Documents/GitHub/TOTK-Armor-Tracker/TotkArmorTracker/TotkArmorTracker.pro -spec linux-g++ CONFIG+=qtquickcompiler
+"/opt/Qt/6.5.2/gcc_64/bin/qmake" ../project/TotkArmorTracker.pro -spec linux-g++ CONFIG+=qtquickcompiler
 "/usr/bin/make" -j4
+
+# If flags are raised to compress outputs, tar all generated files.
+if [ "$1" = "--compress" ]
+then
+    echo Compressing outputs...
+    tar -cf totkArmorTracker.tar *.*
+    mv totkArmorTracker.tar ..
+fi
+
 exit 0
