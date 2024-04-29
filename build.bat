94f23eb5f514d4e7602bc8532931e5fdae0ee928
Noah Black <57203908+ReactedTugBoat@users.noreply.github.com>
2024-04-28 23:01:01 -0400

    Modified build scripts
    - Added command line args for custom QT installation paths
    - Finished build.sh, work started on build.bat

--- build.bat
+++ build.bat
@@ -1,8 +1,25 @@
 @echo off
 
 REM Parse user inputs for command line arguments.
+set "qtdir="
+if not "%1"=="" (
+    if not "%1"=="--compress" (
+        set "qtdir=%1"
+    )
+)
+
+if "%qtdir%"=="" (
+    echo Building with PATH qt installation.
+) else (
+    echo Building with custom qt installation.
+    echo         jom path: %qtdir%/TODO
+    echo       qmake path: %qtdir%/TODO
+    echo windeployqt path: %qtdir%/TODO
+)
+
 set "compress=false"
 if "%1"=="--compress" set "compress=true"
+if "%2"=="--compress" set "compress=true"
 
 REM Initialize bin directory, if not existing.
 echo Generating bin directory...
@@ -11,11 +28,19 @@ cd bin
 
 REM Clean and run qmake/make build steps.
 echo Building new exe...
-call vcvarsall.bat x64
-jom clean
-qmake ../project/TotkArmorTracker.pro -spec win32-msvc "CONFIG+=qtquickcompiler"
-jom -f Makefile qmake_all
-jom
+if "%qtdir%"=="" (
+    call vcvarsall.bat x64
+    jom clean
+    qmake ../project/TotkArmorTracker.pro -spec win32-msvc "CONFIG+=qtquickcompiler"
+    jom -f Makefile qmake_all
+    jom
+) else (
+    call vcvarsall.bat x64
+    jom clean
+    qmake ../project/TotkArmorTracker.pro -spec win32-msvc "CONFIG+=qtquickcompiler"
+    jom -f Makefile qmake_all
+    jom
+)
 
 REM Copy required Qt .dll files and other build-specific files
 copy *.xml release
@@ -23,9 +48,8 @@ windeployqt ./release/TotkArmorTracker.exe --qmldir ../project
 
 REM (OPTIONAL) Compress the final project using 7zip and move to the parent directory.
 if "%compress%"=="true" (
-    "C:\Program Files\7-Zip\7z.exe" a totkArmorTracker_windows.zip .\release\*
-    move /y totkArmorTracker_windows.zip ..
+    "C:\Program Files\7-Zip\7z.exe" a TotkArmorTracker_Windows.zip .\release\*
+    move /y TotkArmorTracker_Windows.zip ..
 )
 
 cd ..
-REM exit 0
